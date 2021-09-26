076f2cf24235c1ea2fb59099da7bece8650bdb77
leidyvega27 <lvegara1@ibero.edu.co>
2021-09-26 15:55:41 -0500

    proyecto

---
+++ script.js
@@ -0,0 +1,118 @@
+$( ".paisE").css("color","purple")
+$(".paisE").mouseleave(
+    function() {
+        $(".paisE").css("color","gray") })
+
+
+$(".paisB").click(function() {
+
+    $(".paisB").css("font-size", "30px")
+}
+)
+$(".paisB").mouseenter(
+    function() {
+        $(".paisB").css("background","blue") })
+
+
+$(".paisC").mouseover(
+    function() {
+        $(".paisC").css("background","orange")
+        
+    }
+)
+
+$(".paisC").mouseout(
+    function() {
+        $(".paisC").css("background","green") })
+
+  $(".PaisZ").dblclick(
+            function() {
+                $(".PaisZ").css("background" ,"red") })
+ $(".PaisZ").click(
+     function() {
+
+       $(".PaisZ").css("font-size" ,"10px") })
+
+
+ $(".paisT").mouseout(
+     function() {$(".paisT").css("background" ,"red") })
+
+ $(".paisT").mouseover( function() { $(".paisT").css("background","blue")})
+
+
+
+  $(".PaisV").dblclick(
+                function() {
+     $(".PaisV").css("color" ,"green") })
+           
+           
+  $(".PaisV").click(
+       function() {
+   $(".PaisV").css("color","purple")})
+
+
+
+ $(".paisH").dblclick(
+    function() {
+$(".paisH").css("color" ,"orange") })
+
+
+$(".paisH").click(
+function() {
+$(".paisH").css("color","white")})
+
+
+
+$(".paisN").dblclick(
+    function() {
+$(".paisN").css("font-size" ,"50px") })
+
+
+$(".paisN").click(
+function() {
+$(".paisN").css("font-size","15px")})
+
+
+$(".paisM").mouseenter(
+    function() {
+$(".paisM").css("background" ,"red") })
+
+
+$(".paisM").mouseout(
+function() {
+$(".paisM").css("background","yellow")})
+
+
+$(".paisL").mouseenter(
+    function() {
+$(".paisL").css("background" ,"pink") })
+
+
+$(".paisL").mouseout(
+function() {
+$(".paisL").css("background","white")})
+
+
+$(".parrafoM").mouseleave(
+    function() {
+    $(".parrafoM").css("background","orange")})
+
+
+    
+$(".parrafoL").click(
+    function() {
+    $(".parrafoM").css("background","blue")})
+
+
+
+$(".TiposC").click(
+    function() {
+    $(".parrafoM").css("background","orange")})
+
+    
+$(".TiposC").click(
+    function() {
+    $(".TiposC").css("font-size","30px")})
+    
+    
+    
